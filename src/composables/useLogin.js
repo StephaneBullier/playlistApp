8aafd97f1415a92cfbc67baf440b8925f9557dd4
steph <sbullier@gmail.com>
2021-01-07 12:12:42 +0100

    Add Login page Add login route Add login functionality

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -2,20 +2,30 @@ import { ref } from 'vue';
 import { projectAuth } from '@/firebase/config';
 
 const error = ref(null)
+const isPending = ref(false)
 
 const login = async (email, password) => {
     error.value = null
+    isPending.value = true
     try {
         const response = await projectAuth.signInWithEmailAndPassword(email, password)
         error.value = null
+        isPending.value = false
+
         return response
     } catch (err) {
-        error.value = 'Incorrect login credentials'
+        if (err.code === 'auth/user-not-found') {
+            error.value = 'Il n\'y a pas d\'enregistrement d\'utilisateur correspondant à cet identifiant.'
+        }
+        else {
+            error.value = err.message
+        }
+        isPending.value = false
     }
 }
 
 const useLogin = () => {
-    return { error, login }
+    return { error, isPending, login }
 }
 
 export default useLogin
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <h3>Login</h3>
+        <input type="email" placeholder="email" v-model="email">
+        <input type="password" placeholder="password" v-model="password">
+        <div class="error" v-if="error">{{error}}</div>
+        <button v-if="!isPending">Log in</button>
+        <button v-if="isPending" disabled>Loading</button>
+    </form>
+</template>
+
+<script>
+import useLogin from '@/composables/useLogin';
+import {ref} from 'vue';
+
+export default {
+    name: "Login",
+    setup() {
+        const { error, isPending, login } = useLogin()
+
+        const email = ref('')
+        const password = ref('')
+
+        const handleSubmit = async () => {
+            return await login(email.value, password.value)
+        }
+
+        return { error, email, password, isPending, handleSubmit }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
