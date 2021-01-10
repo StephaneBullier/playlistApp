141d841880b408970289070fac39d4be69fbde7d
steph <sbullier@gmail.com>
2021-01-10 17:05:17 +0100

    Display title of playlist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
-    <h1>Home</h1>
+    <div class="home">
+        <div class="error" v-if="error">Les données ne peuvent pas être récupérées.</div>
+        <div v-if="documents">
+            <div v-for="playlist in documents" :key="playlist.id">
+                <h2>{{ playlist.title }}</h2>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 export default {
-  name: 'Home',
+    name: 'Home',
+    setup() {
+        const { documents, error } = getCollection('playlists')
+
+        return { documents, error }
+    }
 }
 </script>
