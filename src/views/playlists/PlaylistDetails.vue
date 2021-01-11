c1d77e0f5f5045f1a9f95841c6b8a21fa59220e4
steph <sbullier@gmail.com>
2021-01-11 13:56:29 +0100

    Add details for playlist details page

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,15 +1,72 @@
 <template>
-    <h2>Playlist details</h2>
-    {{ id }}
+    <div class="error" v-if="error">{{ error }}</div>
+    <div v-if="playlist" class="playlist-details">
+        <div class="playlist-info">
+            <div class="cover">
+                <img :src="playlist.coverUrl" alt="">
+            </div>
+            <h2>{{ playlist.title }}</h2>
+            <p>Created by {{ playlist.userName }}</p>
+            <p class="description">{{ playlist.description }}</p>
+        </div>
+        <!-- Song list -->
+        <div class="song-list">
+            Song list
+        </div>
+    </div>
 </template>
 
 <script>
+import getDocument from '@/composables/getDocument'
+
 export default {
     name: 'PlaylistDetails',
-    props: ['id']
+    props: ['id'],
+    setup(props) {
+        const { error, document: playlist } = getDocument('playlists', props.id)
+
+        return { error, playlist }
+    }
 }
 </script>
 
-<style scoped>
-
+<style>
+.playlist-details {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 80px;
+}
+.cover {
+    overflow: hidden;
+    border-radius: 20px;
+    position: relative;
+    padding: 160px;
+}
+.cover img {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 200%;
+    max-height: 200%;
+}
+.playlist-info {
+    text-align: center;
+}
+.playlist-info h2 {
+    text-transform: capitalize;
+    font-size: 28px;
+    margin-top: 20px;
+}
+.playlist-info p {
+    margin-bottom: 20px;
+}
+.username {
+    color: #999;
+}
+.description {
+    text-align: left;
+}
 </style>
