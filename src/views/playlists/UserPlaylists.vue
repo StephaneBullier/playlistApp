c3dc4479d4b23815047ea5d58b7837817ac65fc0
steph <sbullier@gmail.com>
2021-01-12 11:31:56 +0100

    Display the own playlist of current user

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="user-playlists">
-        My playlists
-
+        <h2>My playlists</h2>
+        <div v-if="playlists">
+            <list-view :playlists="playlists" />
+        </div>
     </div>
 </template>
 
 <script>
-
+import ListView from '@/components/ListView';
 import getUser from '@/composables/getUser'
 import getCollection from '@/composables/getCollection';
 
 export default {
     name: "UserPlaylists",
-
+    components: { ListView },
     setup() {
         const { user } = getUser()
         const { documents: playlists } = getCollection(
