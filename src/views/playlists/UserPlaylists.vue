35ae97c05ca2afb2bacd1514d73838ae0fdb0c6d
steph <sbullier@gmail.com>
2021-01-12 11:26:08 +0100

    Add query parameters to fetch playlists of an user

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="user-playlists">
         My playlists
+
     </div>
 </template>
 
 <script>
+
+import getUser from '@/composables/getUser'
+import getCollection from '@/composables/getCollection';
+
 export default {
-    name: "UserPlaylist"
+    name: "UserPlaylists",
+
+    setup() {
+        const { user } = getUser()
+        const { documents: playlists } = getCollection(
+            'playlists',
+            ['userId', '==', user.value.uid]
+        )
+
+        return { playlists }
+    }
 }
 </script>
 
